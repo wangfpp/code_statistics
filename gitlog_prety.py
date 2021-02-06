3d89b51598761b5b233617b3a090a1797ffd77f2
wangfpp <wangfpp@gmail.com>
2021-02-06 18:02:15 +0800

    增加参数解析和py2-3的适配

--- gitlog_prety.py
+++ gitlog_prety.py
@@ -6,6 +6,7 @@ import os
 import re as r
 import sys
 from datetime import datetime, date
+from argparse import ArgumentParser
 import calendar
 
 
@@ -13,10 +14,36 @@ import calendar
 
 
 class StatiCode:
-    def __init__(self, mode, args):
-        self.mode = mode
-        self.argv = args
+    def __init__(self, kwargs):
+        '''
+            这里的函数初始化的参数应该在外部处理  处理函数不负责参数 应该只接受正确的参数进行逻辑处理
+        '''
+        print(kwargs)
         self.enter_bytes = self.getEnterBytes()
+        if not kwargs.git_name:
+            self.git_name = self.getGitUserName()
+        else:
+            self.git_name = kwargs.git_name
+
+        if len(kwargs.branchs) <= 0:
+            self.branchs = self.getAllBranchs()
+        else:
+            self.branchs = kwargs.branchs
+        self.start_date = kwargs.start_date
+        self.mode = kwargs.mode
+        self.gitCount = {}
+        '''
+            {
+                master: {
+                    1: 2000,
+                    2: 3000,
+                    3: 400
+                },
+                datachannel: {
+
+                }
+            }
+        # '''
     
     def main_f(self):
         '''主函数'''
@@ -24,35 +51,88 @@ class StatiCode:
         self.start_date = start_date
         self.now_date = now_date
         self.total_month = total_month
-        self.branchs()
-        
+        self.branchsFn()
+        self.count = {}
+        for branch in self.gitCount:
+            self.count[branch] = {
+                "add": 0,
+                "delete": 0,
+                "nums": 0
+            }
+            for date_key in self.gitCount[branch]:
+                self.count[branch]["add"] += self.gitCount[branch][date_key]["add"]
+                self.count[branch]["delete"] += self.gitCount[branch][date_key]["delete"]
+                self.count[branch]["nums"] += self.gitCount[branch][date_key]["nums"]
+        print(self.count)
+
+    def getGitUserName(self):
+        '''
+            查询本机的git config user.name
+        '''
+        try:
+            git_name = sup.check_output("git config user.name", shell=True)
+            git_name = git_name.strip() # strip 移除字符串头尾指定的字符 默认是空格或换行符
+            if type(git_name) == bytes:
+                git_name = self.byte2str(git_name)
+            return git_name
+        except sup.CalledProcessError as err:
+            print("\033[0;31;40m {} \033[0m".format("查询本机的git username异常"))
+            print("\033[0;31;40m {} \033[0m".format(err))
+            exit(2)
+        print("\033[0;34;40""\033[0m")
+
+    def getAllBranchs(self):
+        '''
+            获取所有的分支(你操作过的分支 执行过git checkout)
+        '''
+        cmd = "git branch"
+        try:
+            project_branchs = sup.check_output(cmd, shell=True)
+            project_branchs = project_branchs.split(self.enter_bytes)
+            return project_branchs
+        except sup.CalledProcessError as err:
+            print("\033[0;31;40m {} \033[0m".format(err))
+            exit(2)
 
     def getEnterBytes(self):
         # 不同平台的回车字符
-        enter_bytes = "\n"
+        enter_bytes = b"\n"
         if(os.name == "nt"):
-            enter_bytes = "\r\n"
+            enter_bytes = b"\r\n"
+            if b"true" in self.crlf():
+                enter_bytes = b"\n"
         # 判断python版本号　a bytes-like object is required, not 'str'
-        return self.py2py3(enter_bytes)
+        return enter_bytes
+    def crlf(self):
+        '''
+            查看是否开启了CRLF
+        '''
+        cmd = "git config core.autocrlf"
+        try:
+            crlf_val = sup.check_output(cmd, shell=True)
+            return crlf_val
+        except sup.CalledProcessError as err:
+            print("\033[0;31;40m {} \033[0m".format(err))
+            exit(2)
 
     def py2py3(self, str):
         py_verison = sys.version_info.major
         if py_verison > 2:
-            str = self.str2bytes(str)
+            str = self.byte2str(str)
         return str
+
+    def byte2str(self, str):
+        return str.decode(encoding='utf-8')
 
     def str2bytes(self, str):
         return str.encode(encoding='utf-8')
 
     def dateAbout(self):
         '''计算日期相关的函数体　返回开始和结束日期以及相差的月数'''
-        _args = self.argv
         now_date = datetime.now()
         now_year = now_date.year
         now_month = now_date.month
-        _start_date = "{}-01-01".format(now_year)
-        if len(_args) > 1 and _args[1] != "":
-            _start_date = _args[1]
+        _start_date = self.start_date
         totla_month = self.totalMonth(_start_date, now_date)
         bk_start_date = datetime.strptime(_start_date, "%Y-%m-%d")
         return [bk_start_date, now_date, totla_month]
@@ -81,60 +161,55 @@ class StatiCode:
         # month = addMonths(current, 1)
         return month
 
-        
-    def branchs(self):
-        try:
-            cmd = "git branch"
-            project_branchs = sup.check_output(cmd, shell=True)
-            project_branchs = project_branchs.split(self.enter_bytes)
-            for branch in project_branchs:
-                if branch != "":
-                    # 以*开头的是当前分支　可以直接进行log查询　其他分支需要checkout
-                    print("\033[0;36;40m===================当前分支为:{}  ========================\033[0m".format(branch))
-                    # m_patt = self.py2py3() 
-                    pattern = r'^\*'
-                    if sys.version_info.major > 2:
-                        pattern = r.compile(b'^\*')
+    def branchsFn(self):
+        for branch in self.branchs:
+            if type(branch) == bytes:
+                branch = self.byte2str(branch)
+            if branch != "":
+                # 以*开头的是当前分支　可以直接进行log查询　其他分支需要checkout
+                # print("\033[0;36;40m===================当前分支为:{}  ========================\033[0m".format(branch))
+                # m_patt = self.py2py3() 
+                pattern = r'^\*'
+                if r.match(pattern, branch):
+                    self.yearCodeNum(branch)
+                else:
+                    try:
+                        switch_branch_cmd = "git checkout {}".format(branch)
+                        switch_result = sup.check_output(switch_branch_cmd, shell=True)
+                        self.yearCodeNum(branch)
+                    except sup.CalledProcessError as err:
+                        print("\033[31m {} \033[0m".format(err))
 
-                    if r.match(pattern, branch):
-                        self.yearCodeNum()
-                    else:
-                        try:
-                            switch_branch_cmd = "git checkout {}".format(branch)
-                            # switch_result = sup.check_output(switch_branch_cmd, shell=True)
-                            # print(switch_result)
-                            self.yearCodeNum()
-                        except sup.CalledProcessError as err:
-                            print("\033[31m {} \033[0m".format(err))
-
-        except sup.CalledProcessError as err:
-            print("\033[0;31;40m {} \033[0m".format(err))
-
-    def yearCodeNum(self):
+    def yearCodeNum(self, branch):
         ''''
         　统计当前分支下一定日期内的日志
         '''
+        if branch not in self.gitCount:
+            self.gitCount[branch] = {}
         for i in range(0, self.total_month):
             start_date = self.addMonths(self.start_date, i)
             next_date = self.addMonths(self.start_date, i+1)
             start_date = start_date.strftime("%Y-%m-%d")
             next_date = next_date.strftime("%Y-%m-%d")
-            self.monthCodeNum(start_date, next_date)
+            date_key = "{}:{}".format(start_date, next_date)
+            if date_key not in self.gitCount[branch]:
+                self.gitCount[branch][date_key] = {}
+            self.monthCodeNum(start_date, next_date, branch, date_key)
 
     
     def logPretty(self, start_date, end_date):
         '''
         查询某期间的日志格式化输出
         '''
-        cmd = "git log --author=wangfpp --since={} --until={}  --pretty=tformat: --shortstat".format(start_date, end_date)
+        cmd = "git log --author={} --since={} --until={}  --pretty=tformat: --shortstat".format(self.git_name, start_date, end_date)
         try:
-            sub_process = sup.check_output(cmd, shell=True)
+            pretty_commit_log = sup.check_output(cmd, shell=True)
         except sup.CalledProcessError as err:
-            sub_process = ""
+            pretty_commit_log = ""
             print("\033[0;31;40m {} \033[0m".format(err))
             # os.system("")
             exit(1)
-        return sub_process.split(self.enter_bytes)
+        return pretty_commit_log.split(self.enter_bytes)
 
     '''
     格式化 log输出并计算代码提交数量
@@ -146,7 +221,7 @@ class StatiCode:
             pattern = r.compile(b'[a-z \(\)\+\-]')
         log_num = r.sub(pattern, b"", log_str).split(b",")
         if self.mode == "detail":
-            print("\033[0;34;40 {} \033[0m".format(log_str))
+            c
         code_num = {
             "file": 0,
             "add": 0,
@@ -159,7 +234,7 @@ class StatiCode:
                     code_num["file"] += item
                 elif (i == 1):
                     if (item >= 1000):
-                        print("\033[0;33;40m　此次提交代码{}可能有第三方依赖库!!!\033[0m".format(item))
+                        print("\033[0;33;40m此次提交代码{} \033[0m".format(item))
                     code_num["add"] += item
                 elif ( i == 2):
                     code_num["delete"] += item
@@ -169,12 +244,13 @@ class StatiCode:
     '''
     计算一段时间内的代码量
     '''
-    def monthCodeNum(self, start_date, next_date):
+    def monthCodeNum(self, start_date, next_date, branch, date_key):
         log_list = self.logPretty(start_date, next_date)
         month_code = {
             "file": 0,
             "add": 0,
-            "delete": 0
+            "delete": 0,
+            "nums": 0
         }
         if(len(log_list) > 0):
             for log_item in log_list:
@@ -182,35 +258,43 @@ class StatiCode:
                 month_code["file"] += code_num["file"]
                 month_code["add"] += code_num["add"]
                 month_code["delete"] += code_num["delete"]
-            print("\033[0;35;40m{}至{}期间您　共有:{}个文件变动 \r\n新增code:{}行 \r\n删除code:{}行 \r\n总计:{}行 \033[4m".format(
-                    start_date,
-                    next_date,
-                    month_code["file"],
-                    month_code["add"],
-                    month_code["delete"],
-                    month_code["add"]-month_code["delete"]
-                    ))
+                month_code["nums"] += 1
+            # print("\033[0;35;40m{}至{}期间您　共有:{}个文件变动 \r\n新增code:{}行 \r\n删除code:{}行 \r\n总计:{}行 \033[4m".format(
+            #         start_date,
+            #         next_date,
+            #         month_code["file"],
+            #         month_code["add"],
+            #         month_code["delete"],
+            #         month_code["add"]-month_code["delete"]
+            #         ))
         else:
-            print("无提交记录")
-        print("\033[0;34;40""\033[0m")
+            pass
+            # print("无提交记录")
+        # print("\033[0;34;40""\033[0m")
+        self.gitCount[branch][date_key]["add"] = month_code["add"]
+        self.gitCount[branch][date_key]["delete"] = month_code["delete"]
+        self.gitCount[branch][date_key]["nums"] = month_code["nums"]
 
 if __name__ == "__main__":
-    argvs = sys.argv
-    mode = "default"
-    if (len(argvs) > 1):
-        date_str = argvs[1]
-        mat = r.match(r"(\d{4}-\d{2}-\d{2})",date_str)
-        if (mat):
-            main = StatiCode(mode, argvs)
-            main.main_f()
-        else:
-            print("\033[0;31;40m {} \033[0m".format("输入的日期格式必须是 2020-01-01"))
-    else:
-        now_date = datetime.now().strftime('%Y-%m-%d')
-        print("""\033[0;31;33m 
-            您没有提供附属参数则从{}开始统计您的代码
-            您可以提供时间则从规定时间统计例如:
-            python ./gitlog_prety.py  2020-11-01 \033[3m""".format(now_date))
-        main = StatiCode(mode, argvs)
-        main.main_f()
+    now_date = datetime.now()
+    now_year_start = "{}-01-01".format(now_date.year)
+    parse = ArgumentParser(description="查询某人某项目一定时间内的代码提交量")
+    parse.add_argument("-mode", "--mode", choices=["default", "detail", "slient"], type=str, default="default", help="是否输出详细信息")
+    parse.add_argument("-name", "--git_name", type=str, help="您的git config user.name")
+    parse.add_argument("-branchs", "--branchs", nargs="+", help="要查询的分支列表", default=[])
+    parse.add_argument("-st", "--start_date", type=str, default=now_year_start, help="查询的起始日期默认当前年的一月一号")
+    
+    arguments = parse.parse_args()
+    mat = r.match(r"(\d{4}-\d{2}-\d{2})",arguments.start_date)
+    if not mat:
+        print("\033[0;31;40m {} \033[0m".format("输入的日期格式必须是 YYYY-MM-dd"))
+        exit(1)
+    main = StatiCode(arguments)
+    main.main_f()
+  
+    '''
+        退出的状态码 
+            1 参数错误
+            2 异常情况
+    '''
     
